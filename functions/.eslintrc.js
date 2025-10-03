2b536b96179d63093608aa4d66f801804d4e3a89
kuga <kuga1486@icloud.com>
2025-10-03 21:39:32 +0900

    ver.3.0. アプリ仕様の大幅な変更。通知エラー

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,19 +1,16 @@
-/* eslint-env node */   // Node.js 環境であることを明示
-/* eslint-disable no-undef */ // require/module を未定義扱いしない
+/* eslint-env node */   // ← これを一行目に入れる
 
 module.exports = {
   env: {
-    node: true,   // Node.js グローバルを有効化
-    es6: true,    // ES6 構文をサポート
+    node: true,
+    es2021: true,
   },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  extends: ["eslint:recommended", "google"],
+  parserOptions: { ecmaVersion: 12 },
+  extends: ["eslint:recommended"],
   rules: {
     quotes: ["error", "double", { allowTemplateLiterals: true }],
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
-    "require-jsdoc": "off", // Googleルールで不要なら無効化
+    "require-jsdoc": "off",
   },
 };
