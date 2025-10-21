7636499389ac4474f0e7fb422d66c7b770c23e73
kuga <kuga1486@icloud.com>
2025-10-21 10:48:08 +0900

    ver 1.1.1 ログイン回数の記録

--- src/changelog.js
+++ src/changelog.js
@@ -1,6 +1,6 @@
 const CHANGELOG = [
   {
-    version: "1.1.0",
+    version: "1.1.1",
     date: "2025-10-21",
     title: "バージョン管理UIの導入",
     items: [
@@ -17,3 +17,10 @@ const CHANGELOG = [
 ];
 
 export default CHANGELOG;
+//バージョンを変える場合の手順
+//このファイルにバージョンの履歴を追記
+//git status
+//git add
+//git commit -m "ver x.x.x 変更内容"
+//git push origin main
+//git tag -a ver1.1.0 -m "feat(analytics): 内容　verのあとに空白を入れない"
