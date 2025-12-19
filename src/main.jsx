53d15ebfea246def69a9ee5c7b2abab99420675f
kuga <kuga1486@icloud.com>
2025-12-19 11:18:38 +0900

    PWA化2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import ReactDOM from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 import App from "./App.jsx";
 import { AuthProvider } from "./hooks/useAuth";
 import "./index.css";   // 既存のスタイル
+
+registerSW();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
